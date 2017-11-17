8ef8ef7a8f20b4137ed4da0a2be25dd1737c2b86
Yining Liang <yil290@ucsd.edu>
2017-11-17 15:54:51 -0800

    Removed a bunch of garbage. Drawing created.

---
+++ Assets/Scripts/DrawingController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRUtils;
+
+public class DrawingController : MySingleton<DrawingController> {
+
+    /// <summary>
+    /// Stores all the drawings.
+    /// </summary>
+    private List<GameObject> drawings;
+
+    /// <summary>
+    /// The prefab to create when a new drawing is created.
+    /// </summary>
+    public GameObject drawingPrefab;
+
+    /// <summary>
+    /// The current drawing to add points to when drawing has been started.
+    /// </summary>
+    private LineRenderer currentDrawing;
+
+    /// <summary>
+    /// Stores the points of a drawing.
+    /// </summary>
+    private List<Vector3> points = new List<Vector3>();
+
+    /// <summary>
+    /// Flags whether drawing has been started.
+    /// </summary>
+    private bool isDrawingStarted = false;
+
+    private void Start()
+    {
+        VRControllerInput.Instance.OnSelectButtonDown += (HandType hand) =>
+        {
+            if (hand == HandType.RIGHT)
+                StartDrawing();
+        };
+        VRControllerInput.Instance.OnSelectButtonUp += (HandType hand) =>
+        {
+            if (hand == HandType.RIGHT)
+                FinishDrawing();
+        };
+    }
+
+    public void StartDrawing()
+    {
+        isDrawingStarted = true;
+        points.Clear();
+        currentDrawing = Instantiate(drawingPrefab, this.transform).GetComponent<LineRenderer>();
+    }
+
+    public void FinishDrawing()
+    {
+        isDrawingStarted = false;
+        currentDrawing = null;
+    }
+
+    private void addPoint(Vector3 pos)
+    {
+        points.Add(pos);
+        currentDrawing.positionCount = points.Count;
+        currentDrawing.SetPositions(points.ToArray());
+    }
+
+    private void Update()
+    {
+        if (isDrawingStarted)
+        {
+            addPoint(VRControllerInput.Instance.GetControllerPosition(HandType.RIGHT));
+        }
+    }
+}
--- Assets/Scripts/VRControllerInput.cs
+++ Assets/Scripts/VRControllerInput.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 using System;
 
+public enum HandType
+{
+    LEFT,
+    RIGHT
+}
+
 namespace VRUtils
 {
     // This class encapsulates all the input required for most VR games.
     // It has events that can be subscribed to by classes that need specific input.
-    public class VRControllerInput : MonoBehaviour
+    public class VRControllerInput : MySingleton<VRControllerInput>
     {
-        public enum HandType
-        {
-            LEFT,
-            RIGHT
-        }
+        /// <summary>
+        /// References to transforms of both controller.
+        /// </summary>
+        [SerializeField] private Transform leftControllerTrans;
+        [SerializeField] private Transform rightControllerTrans;
+
+
         
         /// <summary>
         /// Action for when the select button is pressed down. 
@@ -26,6 +34,25 @@ namespace VRUtils
         /// bool -- true if it is left hand, false otherwise.
         /// </summary>
         public Action<HandType> OnSelectButtonUp;
+
+        /// <summary>
+        /// Get the world position of the controllers.
+        /// </summary>
+        /// <param name="hand">Left/right controller.</param>
+        /// <returns>The position of the controller.</returns>
+        public Vector3 GetControllerPosition(HandType hand)
+        {
+            Vector3 pos = Vector3.zero;
+            if (hand == HandType.LEFT)
+            {
+                pos = leftControllerTrans.transform.position;
+            }
+            else if (hand == HandType.RIGHT)
+            {
+                pos = rightControllerTrans.transform.position;
+            }
+            return pos;
+        }
 
 
         void Update()
