3f31e7cf09438a73712b0aa5172e05d95a4e3381
epokta <epokta@UCSD.EDU>
2017-11-19 19:43:25 -0800

    added line renderer

---
+++ Assets/Scripts/ControllerInput.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        //if(OVRInput.GetDown)
+        if(OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+
+            Debug.Log("right select button is held");
+        
+        }
+
+        if (OVRInput.GetUp(OVRInput.RawButton.RIndexTrigger))
+        {
+
+            Debug.Log("right select button is released");
+
+        }
+    }
+}
---
+++ Assets/Scripts/drawingcontroller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drawingcontroller : MonoBehaviour {
+
+    public GameObject lineRendererobject;
+
+    [SerializeField]private LineRenderer lineRenderer;
+
+    private bool isDrawing;
+
+    // Use this for initialization
+    void Start() {
+
+        if (lineRenderer != null) ;
+
+        Debug.Log("Found this line renderer");
+
+        //lineRenderer = lineRendererobject.GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void StartDrawing()
+    {
+
+        isDrawing = true;
+
+    }
+
+    public void StopDrawing()
+    {
+
+        isDrawing = false;
+
+    }
+
+
+
+
+}
