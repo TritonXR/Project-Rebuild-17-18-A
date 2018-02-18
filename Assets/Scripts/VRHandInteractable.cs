7e78641a16ef3a38b906a3e4809df52ef8d76dfd
Yining Liang <wsfflyn@vip.qq.com>
2018-02-18 12:15:20 -0800

    Created a interactable cube.

---
+++ Assets/Scripts/VRHandInteractable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRHandInteractable : MonoBehaviour {
+
+    //When the hand collider enters this object. aka When the user's hand is over this object.
+    public void OnHandOver()
+    {
+
+    }
+
+    //When the hand collider exits this object. aka When the user's hand releases this object.
+    public void OnHandOut()
+    {
+
+    }
+}
